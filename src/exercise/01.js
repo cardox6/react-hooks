470e696feb376941180a89e27979891caf793e1c
Matias <matias.cardone@code.berlin>
2022-05-11 15:00:06 +0200

    use state, take a prop and use to set initial state

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -2,13 +2,10 @@
 // http://localhost:3000/isolated/exercise/01.js
 
 import * as React from 'react'
-
-function Greeting() {
-  // 💣 delete this variable declaration and replace it with a React.useState call
-  const name = ''
-
+function Greeting({initialName = 'Mati'}) {
+  const [name, setName] = React.useState(initialName)
   function handleChange(event) {
-    // 🐨 update the name here based on event.target.value
+    setName(event.target.value)
   }
 
   return (
